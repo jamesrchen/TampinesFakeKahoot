fadd3ef08d2f1ed8d2f86e5335c5524f22f8b4c5
uncoolbeans <ivcleandedmyroom@gmail.com>
2021-06-19 10:57:09 +0800

    2: Added Variable Answers

---
+++ TampinesFakeKahoot/Questions.swift
@@ -0,0 +1,14 @@
+//
+//  Questions.swift
+//  TampinesFakeKahoot
+//
+//  Created by MMS on 19/6/21.
+//
+
+import Foundation
+
+struct Question {
+    var title: String
+    var answers: Array<String>
+    var correctIndex: Int
+}
